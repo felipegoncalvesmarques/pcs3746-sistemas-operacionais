d30c0536733f2e5bd2309ec56e485d7a9a1edc09
Felipe Goncalves Marques <felipe.goncalves@taqtil.com>
2017-07-22 18:20:16 -0300

    New init and default_cmd

--- docker/default_cmd.sh
+++ docker/default_cmd.sh
@@ -8,18 +8,18 @@ fail() {
 
 SRC=/home/student/src
 #Linux compilation
-# if ! cd $SRC/linux ; then
-#     fail "Mount a linux tree volume under $SRC/linux"
-# fi
+if ! cd $SRC/linux ; then
+    fail "Mount a linux tree volume under $SRC/linux"
+fi
 
-# if [ ! -f .config ] ; then
-#     echo ".config not found, using versatile template"
-#     make ARCH=arm versatile_defconfig
-#     # Enable debug symbols
-#     sed -i -e 's/.*CONFIG_DEBUG_INFO.*/CONFIG_DEBUG_INFO=y/' .config
-#     make ARCH=arm olddefconfig
-# fi
-# make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- all
+if [ ! -f .config ] ; then
+    echo ".config not found, using versatile template"
+    make ARCH=arm versatile_defconfig
+    # Enable debug symbols
+    sed -i -e 's/.*CONFIG_DEBUG_INFO.*/CONFIG_DEBUG_INFO=y/' .config
+    make ARCH=arm olddefconfig
+fi
+make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- all
 
 # Program compilation
 if ! cd $SRC/initramfs ; then
@@ -30,4 +30,5 @@ make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi-
 
 # QEMU Running
 BOOT=$SRC/linux/arch/arm/boot
-qemu-system-arm -s -S -M versatilepb -m 128M -nographic -kernel $BOOT/zImage -dtb $BOOT/dts/versatile-pb.dtb -initrd build/rootfs.gz -append "root=/dev/ram"
+# qemu-system-arm -s -S -M versatilepb -m 128M -nographic -kernel $BOOT/zImage -dtb $BOOT/dts/versatile-pb.dtb -initrd build/rootfs.gz -append "root=/dev/ram"
+qemu-system-arm -M versatilepb -m 128M -nographic -kernel $BOOT/zImage -dtb $BOOT/dts/versatile-pb.dtb -initrd build/rootfs.gz -append "root=/dev/ram"
--- init.c
+++ init.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 int main() {
-	int c;
-	printf("Hello, World!\n");
-	while(1) {
-		c = getchar();
-		printf("teclou %d", c);
+    int pid = fork();
+    int i = 0; 
+    if (pid) {
+        while (1) {
+            i += 3;
+        }
+	} else {
+        while (1) {
+            i += 2;
+        }
 	}
 }
 
